13715b0f5bf666bc294492b467e7ef5dc1d5f531
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-29 13:20:27 -0400

    fixed ts

--- main.ts
+++ main.ts
@@ -11,13 +11,13 @@ basic.showIcon(IconNames.Happy)
 pins.digitalWritePin(DigitalPin.P16, 0)
 
 // LED on
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
   basic.showIcon(IconNames.Yes)
   pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
 // LED off
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+  basic.showIcon(IconNames.No)
+  pins.digitalWritePin(DigitalPin.P16, 0)
 })
