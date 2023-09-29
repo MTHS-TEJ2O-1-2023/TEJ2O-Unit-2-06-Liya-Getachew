5a4994970c2b2bdd1b734837c2eca049f8f79e02
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-29 21:21:56 +0000

    python vers + design

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Liya Getachew
+Created on: Sep 2023
+This module is a Micro:bit MicroPython program that turns an LED on and off.
 """
 
 from microbit import *
 
+"setup"
+display.clear
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+while True:
+    "LED on"
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    "LED off"
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
