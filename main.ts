1e0811d62a7145a1d32b46b45f23301d1d10f9b7
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-29 13:16:04 -0400

    ts vers

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Liya Getachew
+ * Created on: Sep 2023
+ * This program controls an LED with a pin.
 */
 
-basic.showString('Hello, World!')
+// On start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16, 0)
+
+// LED on
+input.onButtonPressed(Button.A, function() {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// LED off
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
